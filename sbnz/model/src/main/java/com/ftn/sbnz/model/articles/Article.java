ced62b547df3a977f4767764add7c147467d9630
Nebojša Vuga <nebojsavuga01@gmail.com>
2024-05-12 18:29:26 +0200

    added first layer article inheritance

--- sbnz/model/src/main/java/com/ftn/sbnz/model/articles/Article.java
+++ sbnz/model/src/main/java/com/ftn/sbnz/model/articles/Article.java
@@ -8,9 +8,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.OneToMany;
 
 @Entity
+@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
 public class Article {
 
     @Id
---
+++ sbnz/model/src/main/java/com/ftn/sbnz/model/articles/FootballArticle.java
@@ -0,0 +1,8 @@
+package com.ftn.sbnz.model.articles;
+
+import javax.persistence.Entity;
+
+@Entity
+public class FootballArticle extends Article{
+    
+}
