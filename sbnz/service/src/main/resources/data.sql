45a04edd95c252a27019c8587fb603d193de6ffd
Nebojša Vuga <nebojsavuga01@gmail.com>
2024-05-13 17:12:57 +0200

    added bibi

--- sbnz/service/src/main/resources/data.sql
+++ sbnz/service/src/main/resources/data.sql
@@ -4,3 +4,6 @@ values (1L, 'nebojsavuga01@gmail.com', 'Nebojsa123', 'Nebojsa', 'Vuga', 'Admin',
 insert into Users
 (id, email, password, first_name, last_name, role, age, gender, height)
 values (2L, 'bogdanjanosevic@gmail.com', 'Bogdan123', 'Bogdan', 'Janosevic', 'User', 22, 'Male', 178.0);
+insert into Users
+(id, email, password, first_name, last_name, role, age, gender, height)
+values (3L, 'dusanbibin@gmail.com', 'Dusan123', 'Dusan', 'Bibin', 'User', 22, 'Male', 183.0);
