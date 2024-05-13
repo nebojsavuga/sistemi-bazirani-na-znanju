c9ac68e65a5be43dbca44f8710b3df7f78deb9b0
Nebojša Vuga <nebojsavuga01@gmail.com>
2024-05-13 17:07:56 +0200

    working!!

--- sbnz/service/src/main/resources/data.sql
+++ sbnz/service/src/main/resources/data.sql
@@ -1,3 +1,3 @@
 insert into Users
-(id, email, password, firstName, lastName, role, age, gender, height)
+(id, email, password, first_name, last_name, role, age, gender, height)
 values (1L, 'nebojsavuga01@gmail.com', 'Nebojsa123', 'Nebojsa', 'Vuga', 'Admin', 22, 'Male', 183.0);
