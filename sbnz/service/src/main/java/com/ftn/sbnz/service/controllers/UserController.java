835621d66406ff5f85df1f4e26929bb05450bf5c
Nebojša Vuga <nebojsavuga01@gmail.com>
2024-05-13 17:53:07 +0200

    slight changes

--- sbnz/service/src/main/java/com/ftn/sbnz/service/controllers/UserController.java
+++ sbnz/service/src/main/java/com/ftn/sbnz/service/controllers/UserController.java
@@ -1,20 +1,15 @@
 package com.ftn.sbnz.service.controllers;
 
 import javax.servlet.http.HttpSession;
-import javax.validation.Valid;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ftn.sbnz.model.users.User;
@@ -24,7 +19,7 @@ import com.ftn.sbnz.service.services.IUserService;
 @RestController
 @RequestMapping(value = "api/users")
 public class UserController {
-	private static Logger log = LoggerFactory.getLogger(UserController.class);
+
 	private IUserService userService;
 
 	@Autowired
