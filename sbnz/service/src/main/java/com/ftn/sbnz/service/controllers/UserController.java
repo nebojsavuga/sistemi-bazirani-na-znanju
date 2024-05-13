a7c1bcb84f384259816ef89907a8a6335f09b192
Nebojša Vuga <nebojsavuga01@gmail.com>
2024-05-13 17:39:36 +0200

    added registration

--- sbnz/service/src/main/java/com/ftn/sbnz/service/controllers/UserController.java
+++ sbnz/service/src/main/java/com/ftn/sbnz/service/controllers/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,6 +32,12 @@ public class UserController {
 	@PutMapping()
 	public ResponseEntity<User> login(@RequestBody LoginDTO loginDTO) {
 		User user = userService.getByEmailAndPassword(loginDTO.email, loginDTO.password);
+		return new ResponseEntity<>(user, HttpStatus.OK);
+	}
+
+	@PostMapping(value = "/register")
+	public ResponseEntity<User> register(@RequestBody RegisterDTO registerDTO) {
+		User user = userService.register(registerDTO);
 		return new ResponseEntity<>(user, HttpStatus.OK);
 	}
 }
--- sbnz/service/src/main/resources/data.sql
+++ sbnz/service/src/main/resources/data.sql
@@ -1,9 +1,9 @@
 insert into Users
-(id, email, password, first_name, last_name, role, age, gender, height)
-values (1L, 'nebojsavuga01@gmail.com', 'Nebojsa123', 'Nebojsa', 'Vuga', 'Admin', 22, 'Musko', 183.0);
+(email, password, first_name, last_name, role, age, gender, height)
+values ( 'nebojsavuga01@gmail.com', 'Nebojsa123', 'Nebojsa', 'Vuga', 'Admin', 22, 'Musko', 183.0);
 insert into Users
-(id, email, password, first_name, last_name, role, age, gender, height)
-values (2L, 'bogdanjanosevic@gmail.com', 'Bogdan123', 'Bogdan', 'Janosevic', 'User', 22, 'Musko', 178.0);
+( email, password, first_name, last_name, role, age, gender, height)
+values ( 'bogdanjanosevic@gmail.com', 'Bogdan123', 'Bogdan', 'Janosevic', 'User', 22, 'Musko', 178.0);
 insert into Users
-(id, email, password, first_name, last_name, role, age, gender, height)
-values (3L, 'dusanbibin@gmail.com', 'Dusan123', 'Dusan', 'Bibin', 'User', 22, 'Musko', 183.0);
+(email, password, first_name, last_name, role, age, gender, height)
+values ( 'dusanbibin@gmail.com', 'Dusan123', 'Dusan', 'Bibin', 'User', 22, 'Musko', 183.0);
