d67605edd6b400dc61224b0278047dcb361e4b5c
BogdanJ33 <bogdanjanosevic@gmail.com>
2024-05-14 20:30:51 +0200

    Implemented adding articles to kieBase

--- sbnz/service/src/main/java/com/ftn/sbnz/service/services/RecommendationService.java
+++ sbnz/service/src/main/java/com/ftn/sbnz/service/services/RecommendationService.java
@@ -1,6 +1,7 @@
 package com.ftn.sbnz.service.services;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import org.kie.api.runtime.KieContainer;
@@ -10,25 +11,40 @@ import org.springframework.stereotype.Service;
 
 import com.ftn.sbnz.model.Filters;
 import com.ftn.sbnz.model.RecommendedArticleDTO;
+import com.ftn.sbnz.model.articles.Article;
+import com.ftn.sbnz.service.repositories.ArticleRepository;
 
 @Service
 public class RecommendationService implements IRecommendationService {
 
 	private final KieContainer kieContainer;
     private Set<RecommendedArticleDTO> recommendations = new HashSet<>();
+    private ArticleRepository articleRepository;
 
     @Autowired
-    public RecommendationService(KieContainer kieContainer) {
+    public RecommendationService(KieContainer kieContainer, ArticleRepository articleRepository) {
         this.kieContainer = kieContainer;
+        this.articleRepository = articleRepository;
     }
 
     @Override
     public Set<RecommendedArticleDTO> getRecommendations(Filters filters) {
+        
         KieSession kieSession = kieContainer.newKieSession("basicKsession");
         RecommendedArticleDTO rc = new RecommendedArticleDTO();
         kieSession.setGlobal("recommendations", recommendations);
         kieSession.insert(rc);
         kieSession.insert(filters);
+        List<Article> allArticles = articleRepository.findAll();
+        for(Article article: allArticles){
+            RecommendedArticleDTO recomendedArticle = new RecommendedArticleDTO();
+            recomendedArticle.brandName = article.getBrandName();
+            recomendedArticle.name = article.getName();
+            recomendedArticle.price = article.getPrice();
+            recomendedArticle.id = article.getId();
+            kieSession.insert(recomendedArticle);
+             
+        }
         kieSession.fireAllRules();
         kieSession.dispose();
         return recommendations;
