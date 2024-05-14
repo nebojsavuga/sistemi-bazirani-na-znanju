c252d3a648f98921e800baf33dd3193825f1d0a1
Nebojša Vuga <nebojsavuga01@gmail.com>
2024-05-14 19:39:47 +0200

    Added kiesession insert

--- sbnz/service/src/main/java/com/ftn/sbnz/service/services/RecommendationService.java
+++ sbnz/service/src/main/java/com/ftn/sbnz/service/services/RecommendationService.java
@@ -1,16 +1,32 @@
 package com.ftn.sbnz.service.services;
 
+import java.util.HashSet;
 import java.util.Set;
+
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.ftn.sbnz.service.controllers.dtos.Filters;
 import com.ftn.sbnz.service.controllers.dtos.RecommendedArticleDTO;
 
 public class RecommendationService implements IRecommendationService {
 
+	private final KieContainer kieContainer;
+    private Set<RecommendedArticleDTO> recommendations = new HashSet<>();
+
+    @Autowired
+    public RecommendationService(KieContainer kieContainer) {
+        this.kieContainer = kieContainer;
+    }
+
     @Override
     public Set<RecommendedArticleDTO> getRecommendations(Filters filters) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getRecommendations'");
+        KieSession kieSession = kieContainer.newKieSession();
+        kieSession.setGlobal("recommendations", recommendations);
+        kieSession.insert(filters);
+        kieSession.fireAllRules();
+        kieSession.dispose();
+        return recommendations;
     }
-    
 }
