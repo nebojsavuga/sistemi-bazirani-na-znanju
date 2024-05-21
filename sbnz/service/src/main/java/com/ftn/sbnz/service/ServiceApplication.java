801805fdb676a3bc79fa3028bb100f5c24f538b8
Nebojša Vuga <nebojsavuga01@gmail.com>
2024-05-21 17:19:49 +0200

    addede templates

--- sbnz/service/src/main/java/com/ftn/sbnz/service/ServiceApplication.java
+++ sbnz/service/src/main/java/com/ftn/sbnz/service/ServiceApplication.java
@@ -1,19 +1,25 @@
 package com.ftn.sbnz.service;
 
-import java.util.Arrays;
+import java.io.InputStream;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.drools.template.DataProviderCompiler;
+import org.drools.template.objects.ArrayDataProvider;
+import org.kie.api.KieBase;
 import org.kie.api.KieServices;
 import org.kie.api.builder.KieScanner;
+import org.kie.api.io.ResourceType;
 import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.kie.internal.utils.KieHelper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
+import org.drools.template.DataProvider;
 
 @EnableJpaRepositories("com.ftn.sbnz.*")
 @ComponentScan(basePackages = { "com.ftn.sbnz.*" })
@@ -34,4 +40,50 @@ public class ServiceApplication {
 		kScanner.start(1000);
 		return kContainer;
 	}
+
+	 @Bean
+    public KieBase forwardKieBase() {
+        InputStream tenisStream = this.getClass().getResourceAsStream("/basic/tenis-template.drt");
+        DataProvider dataProviderTenis = new ArrayDataProvider(new String[][]{
+                {"Tenis", "0", "100", "17.0"},
+                {"Tenis", "100", "108", "19.0"},
+                {"Tenis", "109", "115", "21.0"},
+                {"Tenis", "116", "125", "23.0"},
+                {"Tenis", "126", "140", "25.0"},
+                {"Tenis", "141", "155", "26.0"},
+                {"Tenis", "156", "190", "27.0"},
+                {"Tenis", "191", "250", "27.5"},
+                
+                
+        });
+        DataProviderCompiler converter = new DataProviderCompiler();
+        String tenisDrl = converter.compile(dataProviderTenis, tenisStream);
+
+        InputStream weightStream = this.getClass().getResourceAsStream("/basic/weight-template.drt");
+        DataProvider dataProviderWeight = new ArrayDataProvider(new String[][]{
+                {"DizanjeTegova", "Rekreativac", "Musko", "0.0", "15.0"},
+                {"DizanjeTegova", "Amater", "Musko", "0.0", "45.0"},
+                {"DizanjeTegova", "Pro", "Musko", "0.0", "60.0"},
+                {"DizanjeTegova", "Rekreativac", "Zensko", "0.0", "10.0"},
+                {"DizanjeTegova", "Amater", "Zensko", "0.0", "30.0"},
+                {"DizanjeTegova", "Pro", "Zensko", "0.0", "45.0"},
+                
+        });
+        String weightDrl = converter.compile(dataProviderWeight, weightStream);
+
+        KieHelper kieHelper = new KieHelper();
+        kieHelper.addContent(tenisDrl, ResourceType.DRL);
+        kieHelper.addContent(weightDrl, ResourceType.DRL);
+
+        KieServices kieServices = KieServices.Factory.get();
+        kieHelper.addResource(kieServices.getResources().newClassPathResource("basic/templates.drl"), ResourceType.DRL);
+
+        KieBase kieBase = kieHelper.build();
+        return kieBase;
+    }
+
+    @Bean
+    public KieSession templateKsession() {
+        return forwardKieBase().newKieSession();
+    }
 }
