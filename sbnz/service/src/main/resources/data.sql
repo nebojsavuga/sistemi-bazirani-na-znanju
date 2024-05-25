023ef2d71dc11c2a30deef6423b27dbe65b70fd2
Nebojša Vuga <nebojsavuga01@gmail.com>
2024-05-25 10:51:27 +0200

    added all ruyles

--- sbnz/service/src/main/resources/data.sql
+++ sbnz/service/src/main/resources/data.sql
@@ -27,9 +27,9 @@ values(2, 3, '2023-08-18');
 insert into Concrete_Injury(user_id, injury_id, execution_time)
 values(2, 3, '2023-08-18');
 insert into Concrete_Injury(user_id, injury_id, execution_time)
-values(2, 4, '2023-08-18');
+values(2, 3, '2023-08-18');
 insert into Concrete_Injury(user_id, injury_id, execution_time)
-values(2, 4, '2023-07-18');
+values(2, 3, '2023-07-18');
 
 INSERT INTO Article (name, price, gender, brand_name, DTYPE, sport) 
 VALUES ('Wilson Pro Overgrip', 5.99, 'Unisex', 'Wilson', 'Grip', 'Tenis');
