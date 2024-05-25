8de893fe15ab839bc86a2af20ccb87ee99e1f952
Nebojša Vuga <nebojsavuga01@gmail.com>
2024-05-25 22:35:15 +0200

    generated necesary components

--- recommendation-front/src/app/features/features.module.ts
+++ recommendation-front/src/app/features/features.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FeaturesRoutingModule } from './features-routing.module';
+import { FiltersComponent } from './filters/filters.component';
+import { ArticlesComponent } from './articles/articles.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    FiltersComponent,
+    ArticlesComponent
+  ],
   imports: [
     CommonModule,
     FeaturesRoutingModule
