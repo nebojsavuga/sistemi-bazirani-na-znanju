86aa7e77f992863a1f9d2fd34c8947ed9544c436
Nebojša Vuga <nebojsavuga01@gmail.com>
2024-05-30 18:03:23 +0200

    updated to query params

--- recommendation-front/src/app/core/services/recomendation.service.ts
+++ recommendation-front/src/app/core/services/recomendation.service.ts
@@ -4,6 +4,7 @@ import { Observable, filter } from 'rxjs';
 import { Filter } from '../../shared/models/filter';
 import { environment } from '../../../environment/environment';
 import { RecommendedArticle } from '../../shared/models/articles';
+import { HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -13,59 +14,130 @@ export class RecomendationService {
   constructor(private http: HttpClient) {
   }
 
+  toQueryString(filters: Filter): string {
+    let params = new HttpParams();
+  
+    if (filters.age !== undefined) {
+      params = params.append('age', filters.age.toString());
+    }
+    if (filters.height !== undefined) {
+      params = params.append('height', filters.height.toString());
+    }
+    if (filters.injuries && filters.injuries.length > 0) {
+      filters.injuries.forEach(injury => {
+        params = params.append('injuries', injury.toString());
+      });
+    }
+    if (filters.level !== undefined) {
+      params = params.append('level', filters.level);
+    }
+    if (filters.sport !== undefined) {
+      params = params.append('sport', filters.sport);
+    }
+    if (filters.typeOfFotballPlayer !== undefined) {
+      params = params.append('typeOfFotballPlayer', filters.typeOfFotballPlayer);
+    }
+    if (filters.typeOfFootball !== undefined) {
+      params = params.append('typeOfFootball', filters.typeOfFootball);
+    }
+    if (filters.typeOfRace !== undefined) {
+      params = params.append('typeOfRace', filters.typeOfRace);
+    }
+    if (filters.typeOfField !== undefined) {
+      params = params.append('typeOfField', filters.typeOfField);
+    }
+    if (filters.typeOfWeightlifting !== undefined) {
+      params = params.append('typeOfWeightlifting', filters.typeOfWeightlifting);
+    }
+    if (filters.gender !== undefined) {
+      params = params.append('gender', filters.gender);
+    }
+  
+    // Filter out invalid combinations based on the sport
+    switch (filters.sport) {
+      case 'Fudbal':
+        params = params.delete('typeOfField')
+          .delete('typeOfWeightlifting')
+          .delete('typeOfRace');
+        break;
+      case 'Tenis':
+        params = params.delete('typeOfFootball')
+          .delete('typeOfFotballPlayer')
+          .delete('typeOfWeightlifting')
+          .delete('typeOfRace');
+        break;
+      case 'Orijentiring':
+        params = params.delete('typeOfFootball')
+          .delete('typeOfFotballPlayer')
+          .delete('typeOfWeightlifting')
+          .delete('typeOfField');
+        break;
+      case 'DizanjeTegova':
+        params = params.delete('typeOfFootball')
+          .delete('typeOfFotballPlayer')
+          .delete('typeOfField')
+          .delete('typeOfRace');
+        break;
+    }
+  
+    return params.toString();
+  }
+
   recommend(filters: Filter): Observable<RecommendedArticle[]> {
     filters.age = Number(filters.age);
     filters.height = Number(filters.height);
-    if(filters.age === 0){
+    if (filters.age === 0) {
       filters.age = undefined;
     }
-    if(filters.height === 0){
+    if (filters.height === 0) {
       filters.height = undefined;
     }
-    if(filters.injuries.length === 0){
+    if (filters.injuries.length === 0) {
       filters.injuries = undefined;
     }
-    if(filters.level === ""){
+    if (filters.level === "") {
       filters.level = undefined;
     }
-    if(filters.typeOfField === ""){
+    if (filters.typeOfField === "") {
       filters.typeOfField = undefined;
     }
-    if(filters.typeOfFootball === ""){
+    if (filters.typeOfFootball === "") {
       filters.typeOfFootball = undefined;
     }
-    if(filters.typeOfFotballPlayer === ""){
+    if (filters.typeOfFotballPlayer === "") {
       filters.typeOfFotballPlayer = undefined;
     }
-    if(filters.typeOfRace === ""){
+    if (filters.typeOfRace === "") {
       filters.typeOfRace = undefined;
     }
-    if(filters.typeOfWeightlifting === ""){
+    if (filters.typeOfWeightlifting === "") {
       filters.typeOfWeightlifting = undefined;
     }
-    if(filters.gender === ""){
+    if (filters.gender === "") {
       filters.gender = undefined;
     }
-    if(filters.sport === 'Fudbal'){
+    if (filters.sport === 'Fudbal') {
       filters.typeOfField = undefined;
       filters.typeOfWeightlifting = undefined;
       filters.typeOfRace = undefined;
-    } else if(filters.sport === 'Tenis'){
+    } else if (filters.sport === 'Tenis') {
       filters.typeOfFootball = undefined;
       filters.typeOfFotballPlayer = undefined;
       filters.typeOfWeightlifting = undefined;
       filters.typeOfRace = undefined;
-    }else if(filters.sport === 'Orijentiring'){
+    } else if (filters.sport === 'Orijentiring') {
       filters.typeOfFootball = undefined;
       filters.typeOfFotballPlayer = undefined;
       filters.typeOfWeightlifting = undefined;
       filters.typeOfField = undefined;
-    }else if(filters.sport === 'DizanjeTegova'){
+    } else if (filters.sport === 'DizanjeTegova') {
       filters.typeOfFootball = undefined;
       filters.typeOfFotballPlayer = undefined;
       filters.typeOfField = undefined;
       filters.typeOfRace = undefined;
     }
-    return this.http.put<RecommendedArticle[]>(environment.apiHost + 'recommendation', filters);
+    const queryString = this.toQueryString(filters);
+    const url = `${environment.apiHost}recommendation?${queryString}`;
+    return this.http.get<RecommendedArticle[]>(url);
   }
 }
--- sbnz/service/src/main/java/com/ftn/sbnz/service/controllers/RecommendationController.java
+++ sbnz/service/src/main/java/com/ftn/sbnz/service/controllers/RecommendationController.java
@@ -1,5 +1,6 @@
 package com.ftn.sbnz.service.controllers;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -12,11 +13,19 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ftn.sbnz.model.Filters;
 import com.ftn.sbnz.model.RecommendedArticleDTO;
 import com.ftn.sbnz.model.sport.Sport;
+import com.ftn.sbnz.model.sport.SportFootballPlayer;
+import com.ftn.sbnz.model.sport.SportFootballType;
+import com.ftn.sbnz.model.sport.SportLevel;
+import com.ftn.sbnz.model.sport.SportOrientiringType;
+import com.ftn.sbnz.model.sport.SportTenisField;
+import com.ftn.sbnz.model.sport.SportWeightliftingType;
+import com.ftn.sbnz.model.users.Gender;
 import com.ftn.sbnz.service.config.JwtUtils;
 import com.ftn.sbnz.service.controllers.dtos.BrandNamesDTO;
 import com.ftn.sbnz.service.services.IRecommendationService;
@@ -34,14 +43,37 @@ public class RecommendationController {
         this.recommendationService = recommendationService;
     }
 
-    @PutMapping()
-    public ResponseEntity<Set<RecommendedArticleDTO>> recommend(@RequestBody Filters filters,
+    @GetMapping()
+    public ResponseEntity<Set<RecommendedArticleDTO>> recommend(
+            @RequestParam(value = "gender", required = false) Gender gender,
+        @RequestParam(value = "height", required = false) Float height,
+        @RequestParam(value = "age", required = false) Integer age,
+        @RequestParam(value = "injuries", required = false) Set<Long> injuries,
+        @RequestParam(value = "level", required = false) SportLevel level,
+        @RequestParam(value = "sport", required = true) Sport sport,
+        @RequestParam(value = "typeOfFootballPlayer", required = false) SportFootballPlayer typeOfFotballPlayer,
+        @RequestParam(value = "typeOfFootball", required = false) SportFootballType typeOfFootball,
+        @RequestParam(value = "typeOfRace", required = false) SportOrientiringType typeOfRace,
+        @RequestParam(value = "typeOfField", required = false) SportTenisField typeOfField,
+        @RequestParam(value = "typeOfWeightlifting", required = false) SportWeightliftingType typeOfWeightlifting,
             @RequestHeader(value = "Authorization", required = false) String token) {
         Long userId = null;
         if (token != null && !token.isEmpty()) {
             String jwtt = token.substring(7);
             userId = jwt.getId(jwtt);
         }
+        Filters filters = new Filters();
+        filters.gender = gender;
+        filters.height = height != null ? height : 0.0f;
+        filters.age = age != null ? age : 0;
+        filters.injuries = injuries != null ? injuries : new HashSet<>();
+        filters.level = level;
+        filters.sport = sport;
+        filters.typeOfFotballPlayer = typeOfFotballPlayer;
+        filters.typeOfFootball = typeOfFootball;
+        filters.typeOfRace = typeOfRace;
+        filters.typeOfField = typeOfField;
+        filters.typeOfWeightlifting = typeOfWeightlifting;
         return new ResponseEntity<>(this.recommendationService.getRecommendations(filters, userId), HttpStatus.OK);
     }
 
