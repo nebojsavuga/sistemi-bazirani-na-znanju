2ecd9d6461ea2f37949ede295622939fa90d568f
Nebojša Vuga <nebojsavuga01@gmail.com>
2024-06-02 19:34:19 +0200

    more styling fixes

--- recommendation-front/src/app/features/favorite-display/favorite-display.component.ts
+++ recommendation-front/src/app/features/favorite-display/favorite-display.component.ts
@@ -12,7 +12,10 @@ import { SnackbarService } from '../../core/services/snackbar.service';
 export class FavoriteDisplayComponent implements OnInit {
 
   articles: RecommendedArticle[] = [];
-
+  totalSimilarArticles: RecommendedArticle[] = [];
+  sliceIndex = 0;
+  disableShowMore = false;
+  isLoggedIn = false;
   constructor(private articleService: ArticleService,
     private snackBarService: SnackbarService
   ) { }
@@ -26,12 +29,22 @@ export class FavoriteDisplayComponent implements OnInit {
       {
         next: res => {
           this.articles = res;
+          this.totalSimilarArticles = this.articles.slice(0, 4 * (this.sliceIndex + 1));
         },
         error: err => {
           this.snackBarService.showSnackBar('Došlo je do greške prilikom učitavanja omiljenih artikala.', 'Ok');
         }
       }
     );
+  }
+
+  showMore() {
+    this.sliceIndex += 1;
+    this.disableShowMore = false;
+    if (this.totalSimilarArticles.length >= this.articles.length - 4) {
+      this.disableShowMore = true;
+    }
+    this.totalSimilarArticles = this.articles.slice(0, 4 * (this.sliceIndex + 1));
   }
 
   deleteArticle(event: any) {
